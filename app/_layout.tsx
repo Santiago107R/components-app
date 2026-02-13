c4fe931999f706c7b7b9177e6765220e7eb4af01
Ssanti <santiagorobles107k@gmail.com>
2026-02-13 14:05:56 -0300

    First commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,33 @@
+import { useColorScheme } from '@/hooks/use-color-scheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+import "@/global.css";
+import { useThemeColor } from '@/hooks/use-theme-color';
+import ThemeView from '@/presentation/shared/ThemeView';
+import ThemeText from '@/presentation/ThemeText';
 
 export default function RootLayout() {
+  const backgroundColor = useThemeColor({}, 'background')
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ backgroundColor: backgroundColor, flex: 1 }}>
+
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+
+        <ThemeView margin >
+          <ThemeText type='h1' className='mt-10'>Hola mundo</ThemeText>
+        </ThemeView>
+
+        {/* <Stack>
+
+        </Stack> */}
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
