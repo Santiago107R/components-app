9c2207e3a245b255983d3da3899e4f9bd3513c78
Santiago107R <santiagorobles107k@gmail.com>
2026-02-17 12:16:32 -0300

    third commit

--- app/animation-101/index.tsx
+++ app/animation-101/index.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from 'react-native';
+import useAnimation from '@/hooks/useAnimation';
+import ThemedButton from '@/presentation/shared/ThemedButton';
+import ThemeView from '@/presentation/shared/ThemeView';
+import { useRef } from 'react';
+import { Animated, Easing, View } from 'react-native';
 
 const Animation101Screen = () => {
+
+  const {animatedOpacity, animatedTop, fadeIn, fadeOut, startMovingFromTop, movingToTop} = useAnimation()
+  // const animatedOpacity = useRef(new Animated.Value(0)).current
+  // const animatedTop = useRef(new Animated.Value(-100)).current
+
   return (
-    <View>
-      <Text>Animation101Screen</Text>
-    </View>
+    <ThemeView margin className='justify-center items-center flex-1'>
+
+      <Animated.View
+        className='bg-light-secondary dark:bg-light-secondary rounded-xl'
+        style={{
+          width: 150,
+          height: 150,
+          opacity: animatedOpacity,
+          transform: [{
+            translateY: animatedTop,
+          }]
+        }}
+      />
+
+      <ThemedButton
+        className='my-5'
+        onPress={() => {
+          fadeIn({});
+          startMovingFromTop({easing: Easing.elastic(1)});
+        }}
+      >FadeIn</ThemedButton>
+      <ThemedButton
+        className='my-5'
+        onPress={() => {
+          fadeOut({});
+          movingToTop({callback: () => animatedTop.resetAnimation()});
+        }}
+      >FadeOut</ThemedButton>
+    </ThemeView>
   );
 };
 export default Animation101Screen;
---
+++ hooks/useAnimation.tsx
@@ -0,0 +1,60 @@
+import { View, Text, Animated, Easing } from 'react-native'
+import React, { useRef } from 'react'
+
+const useAnimation = () => {
+    const animatedOpacity = useRef(new Animated.Value(0)).current
+    const animatedTop = useRef(new Animated.Value(0)).current
+
+    const fadeIn = ({duration = 300, toValue = 1, useNativeDriver = true, easing = Easing.linear, callback = () => {},}) => {
+        Animated.timing(animatedOpacity, {
+            toValue: toValue,
+            duration: duration,
+            useNativeDriver: useNativeDriver,
+            easing: easing
+        }).start(callback);
+    }
+
+    const fadeOut = ({duration = 300, toValue = 0, useNativeDriver = true, easing = Easing.ease, callback = () => {},}) => {
+        Animated.timing(animatedOpacity, {
+            toValue: toValue,
+            duration: duration,
+            useNativeDriver: useNativeDriver,
+            easing: easing,
+        }).start(callback);
+    }
+
+    const startMovingFromTop = ({initialPosition = -100 , duration = 700, toValue = 0, useNativeDriver = true, easing = Easing.ease, callback = () => {},}) => {
+        
+        animatedTop.setValue(initialPosition)
+
+        Animated.timing(animatedTop, {
+            toValue: toValue,
+            duration: duration,
+            useNativeDriver: useNativeDriver,
+            // easing: Easing.elastic(1),
+            easing: easing,
+        }).start(callback)
+    }
+
+    const movingToTop = ({duration = 700, toValue = -100, useNativeDriver = true, easing = Easing.ease, callback = () => {},}) => {
+        Animated.timing(animatedTop, {
+            toValue: toValue,
+            duration: duration,
+            useNativeDriver: useNativeDriver,
+            easing: easing
+            // }).start(() => animatedTop.setValue(-100))
+        }).start(callback)
+    }
+
+    return {
+        animatedOpacity,
+        animatedTop,
+
+        fadeIn,
+        fadeOut,
+        startMovingFromTop,
+        movingToTop,
+    }
+}
+
+export default useAnimation
